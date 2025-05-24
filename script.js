6df0c08f83f8e14cd0f327d414774d75b9ef3bf1
saniyapandey2603 <shambhavipandey2233@gmail.com>
2025-05-24 23:08:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function showLove() {
+    document.getElementById('surprise').classList.remove('hidden');
+  }
+
+function flipCard() {
+    document.getElementById('flipCard').classList.toggle('flipped');
+}  
