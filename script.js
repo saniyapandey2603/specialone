8d53c467b7c64d916ac16ba218384d0be0aa22ee
saniyapandey2603 <shambhavipandey2233@gmail.com>
2025-05-25 00:54:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,9 @@ function showLove() {
 function flipCard() {
     document.getElementById('flipCard').classList.toggle('flipped');
 }  
+  document.addEventListener('click', () => {
+    const audio = document.querySelector('.hidenn');
+    if (audio && audio.paused) {
+      audio.play().catch(e => console.log("Autoplay blocked:", e));
+    }
+  }, { once: true });
